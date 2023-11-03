7c2357ef35f8b1be5f43ed9fcf4d8fdb5bdd8ee5
Arda Bakır <abakir@carex.ai>
2023-11-03 15:40:59 +0300

    Create main and update buildspec file

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	path := request.Resource
+	var response events.APIGatewayProxyResponse
+	var err error
+	switch path {
+	case "/add-product":
+		fmt.Println("add product")
+	case "/remove-product":
+		fmt.Println("remove product")
+	case "/empty-cart":
+		fmt.Println("remove all")
+	case "/update-product":
+		fmt.Println("update product")
+	case "/get-cart":
+		fmt.Println("get cart data")
+	default:
+		fmt.Println("not valid")
+	}
+	return response, err
+}
+
+func main() {
+	lambda.Start(handler)
+}
