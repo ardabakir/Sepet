70742b61d27c8a63e5becf6d220678baa716d2fa
Arda Bakır <abakir@carex.ai>
2023-11-05 15:45:47 +0300

    Update responses

--- Service/main.go
+++ Service/main.go
@@ -8,26 +8,41 @@ import (
 )
 
 func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	path := request.Path
-	fmt.Println(path)
-	fmt.Println(request.Resource)
+	path := request.Resource
 	var response events.APIGatewayProxyResponse
 	var err error
 	switch path {
 	case "/add-product":
 		fmt.Println("add product")
+		response.StatusCode = 200
+		response.Body = "Add product"
+		return response, err
 	case "/remove-product":
 		fmt.Println("remove product")
+		response.StatusCode = 200
+		response.Body = "Remove product"
+		return response, err
 	case "/empty-cart":
 		fmt.Println("remove all")
+		response.StatusCode = 200
+		response.Body = "Remove All"
+		return response, err
 	case "/update-product":
 		fmt.Println("update product")
+		response.StatusCode = 200
+		response.Body = "Update product"
+		return response, err
 	case "/get-cart":
 		fmt.Println("get cart data")
+		response.StatusCode = 200
+		response.Body = "Get Cart Data"
+		return response, err
 	default:
 		fmt.Println("not valid")
+		response.StatusCode = 400
+		response.Body = "Not valid"
+		return response, err
 	}
-	return response, err
 }
 
 func main() {
