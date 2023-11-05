f98a7c208a76a5b849d5886d496b5a5de404d486
Arda Bakır <abakir@carex.ai>
2023-11-05 15:46:33 +0300

    Create function for adding product to cart

---
+++ Service/db.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"os"
+	"sepet/Models"
+)
+
+func CreateConnection() (*dynamodb.DynamoDB, error) {
+	region := "eu-central-1"
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(region)},
+	)
+	if err != nil {
+		return nil, err
+	}
+	return dynamodb.New(sess), nil
+}
+
+func AddProductToCart(cartId int, productInfo Models.CartItem) error {
+	conn, err := CreateConnection()
+
+	if err != nil {
+		return err
+	}
+	getInput := dynamodb.GetItemInput{
+		Key: map[string]*dynamodb.AttributeValue{
+			"cartId": {
+				N: aws.String(string(cartId)),
+			},
+		},
+		TableName: aws.String(os.Getenv("cart-table")),
+	}
+	result, getErr := conn.GetItem(&getInput)
+	if getErr != nil {
+		return getErr
+	}
+	var cart *Models.Cart
+	if err := dynamodbattribute.UnmarshalMap(result.Item, &cart); err != nil {
+		return err
+	}
+	updatedItems := append(cart.CartItems, productInfo)
+	attrVal, attrErr := dynamodbattribute.MarshalMap(updatedItems)
+	if attrErr != nil {
+		return attrErr
+	}
+	putInput := dynamodb.PutItemInput{
+		Item:      attrVal,
+		TableName: aws.String(os.Getenv("cart-table")),
+	}
+	_, putErr := conn.PutItem(&putInput)
+	if putErr != nil {
+		return putErr
+	}
+
+	return nil
+}
