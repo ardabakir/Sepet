7ec7df290955680c7510353637957e158990cb7d
Arda Bakır <abakir@carex.ai>
2023-11-06 02:14:46 +0300

    refactor and debug

--- Database/DynamoDB.go
+++ Database/DynamoDB.go
@@ -1,6 +1,7 @@
 package Database
 
 import (
+	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
@@ -18,7 +19,8 @@ func NewDynamoDB(table string, conn *dynamodb.DynamoDB) *DynamoDB {
 	}
 }
 
-func (dyn *DynamoDB) GetItem(keyName string, key string, castTo interface{}) error {
+func (dyn *DynamoDB) GetItem(key string, keyName string, castTo interface{}) error {
+	fmt.Println("before get input")
 	getInput := dynamodb.GetItemInput{
 		Key: map[string]*dynamodb.AttributeValue{
 			keyName: {
@@ -27,29 +29,35 @@ func (dyn *DynamoDB) GetItem(keyName string, key string, castTo interface{}) err
 		},
 		TableName: aws.String(dyn.table),
 	}
+	fmt.Println("got input")
 	result, getErr := dyn.conn.GetItem(&getInput)
 	if getErr != nil {
 		return getErr
 	}
+	fmt.Println("got item")
 	if err := dynamodbattribute.UnmarshalMap(result.Item, &castTo); err != nil {
 		return err
 	}
+	fmt.Println("unmarshaled map")
 	return nil
 }
 
 func (dyn *DynamoDB) PutItem(item interface{}) error {
+	fmt.Println("marshal item")
 	attrVal, attrErr := dynamodbattribute.MarshalMap(item)
 	if attrErr != nil {
 		return attrErr
 	}
+	fmt.Println("marshaled item")
 	putInput := dynamodb.PutItemInput{
 		Item:      attrVal,
 		TableName: aws.String(dyn.table),
 	}
+	fmt.Println("put input")
 	_, putErr := dyn.conn.PutItem(&putInput)
 	if putErr != nil {
 		return putErr
 	}
-
+	fmt.Println("put complete")
 	return nil
 }
